156713d8243e3f0b3518dfcd4af4c7bea94c907b
monkeypineapple <davidrosenholz@gmail.com>
2020-01-21 15:57:14 -0800

    User#new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,27 @@
 class UsersController < ApplicationController
-    def show
+    before_action :authorized, except: [:new, :create]
+    before_action :current_user, except: [:new, :create]
 
+    def index
+        @users = User.all
+    end
+    def show
+        @user = User.find(params[:id])
     end
 
     def new
-
+        @user = User.new
     end
 
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to @user
+        else 
+          flash[:errors] = @user.errors.full_messages
+          render :new
+        end 
     end
 
     def edit
@@ -16,11 +29,19 @@ class UsersController < ApplicationController
     end
 
     def update
-        
+
     end
 
-    def delete
+    def destroy
+        @current_user.destroy
+        session[:user_id] = nil
+        redirect_to home_path
+      end
 
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :age, :city, :occupation, :bio)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :topics
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'users#new', as: 'signup'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/', to: 'sessions#new', as: 'home'
+  post '/login', to: 'sessions#create', as: 'login'
+  delete '/delete_account', to: 'users#destroy', as: 'delete_account'
 end
